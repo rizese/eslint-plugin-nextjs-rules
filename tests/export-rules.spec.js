015644a3201251bc23714bf319318cbc6e993d15
Ricky <ricky+zese@thirtymadison.com>
2023-03-15 11:26:51 -0400

    Version bump

--- tests/export-rules.spec.js
+++ tests/export-rules.spec.js
@@ -1,14 +1,14 @@
 // Import the file that we want to test
-const testsIndex = require('../lib/index');
+const testsIndex = require("../lib/index");
+const { expect } = require("jest");
 
-describe('testsIndex', () => {
+describe("testsIndex", () => {
   test('should export an object named "rules"', () => {
-
     // Check that the file exports exactly one thing
     expect(Object.keys(testsIndex)).toHaveLength(1);
 
     // Check that the key of the exported thing is "rules"
-    expect(Object.keys(testsIndex)[0]).toBe('rules');
+    expect(Object.keys(testsIndex)[0]).toBe("rules");
   });
 
   test('should have a "rules" object with the "no-get-server-side-props" rule', () => {
@@ -16,6 +16,6 @@ describe('testsIndex', () => {
     expect(testsIndex.rules).toBeDefined();
 
     // Check that the "no-get-server-side-props" rule exists in the "rules" object
-    expect(testsIndex.rules['no-get-server-side-props']).toBeDefined();
+    expect(testsIndex.rules["no-get-server-side-props"]).toBeDefined();
   });
 });
